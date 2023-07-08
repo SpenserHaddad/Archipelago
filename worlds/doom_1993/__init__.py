2d94a3f171f783c755c14c3a9df8193595f91914
Spenser Haddad <spenser.haddad@gmail.com>
2023-07-07 22:08:09 -0400

    Fix crash when allow_death_logic is enabled

--- worlds/doom_1993/__init__.py
+++ worlds/doom_1993/__init__.py
@@ -3,7 +3,6 @@ import logging
 from typing import Any, Dict, List
 
 from BaseClasses import CollectionState, Item, ItemClassification, Location, MultiWorld, Region, Tutorial
-from Options import ExcludeLocations
 from worlds.AutoWorld import WebWorld, World
 from . import Events, Items, Locations, Options, Regions, Rules
 
@@ -19,14 +18,16 @@ class DOOM1993Item(Item):
 
 
 class DOOM1993Web(WebWorld):
-    tutorials = [Tutorial(
-        "Multiworld Setup Guide",
-        "A guide to setting up the DOOM 1993 randomizer connected to an Archipelago Multiworld",
-        "English",
-        "setup_en.md",
-        "setup/en",
-        ["Daivuk"]
-    )]
+    tutorials = [
+        Tutorial(
+            "Multiworld Setup Guide",
+            "A guide to setting up the DOOM 1993 randomizer connected to an Archipelago Multiworld",
+            "English",
+            "setup_en.md",
+            "setup/en",
+            ["Daivuk"],
+        )
+    ]
     theme = "dirt"
 
 
@@ -35,6 +36,7 @@ class DOOM1993World(World):
     Developed by id Software, and originally released in 1993, DOOM pioneered and popularized the first-person shooter,
     setting a standard for all FPS games.
     """
+
     option_definitions = Options.options
     game = "DOOM 1993"
     web = DOOM1993Web()
@@ -47,11 +49,7 @@ class DOOM1993World(World):
     location_name_to_id = {data["name"]: loc_id for loc_id, data in Locations.location_table.items()}
     location_name_groups = Locations.location_name_groups
 
-    starting_level_for_episode: List[str] = [
-        "Hangar (E1M1)",
-        "Deimos Anomaly (E2M1)",
-        "Hell Keep (E3M1)"
-    ]
+    starting_level_for_episode: List[str] = ["Hangar (E1M1)", "Deimos Anomaly (E2M1)", "Hell Keep (E3M1)"]
 
     # Item ratio that scales depending on episode count. These are the ratio for 3 episode.
     items_ratio: Dict[str, float] = {
@@ -65,7 +63,7 @@ class DOOM1993World(World):
         "Box of bullets": 13,
         "Box of rockets": 13,
         "Box of shotgun shells": 13,
-        "Energy cell pack": 10
+        "Energy cell pack": 10,
     }
 
     def __init__(self, world: MultiWorld, player: int):
@@ -96,11 +94,14 @@ class DOOM1993World(World):
         # Create regions and locations
         for region_name in Regions.regions:
             region = Region(region_name, self.player, self.multiworld)
-            region.add_locations({
-                loc["name"]: (loc_id if loc["index"] != -1 else None)
-                for loc_id, loc in Locations.location_table.items()
-                if loc["region"] == region_name and self.included_episodes[loc["episode"] - 1]
-            }, DOOM1993Location)
+            region.add_locations(
+                {
+                    loc["name"]: (loc_id if loc["index"] != -1 else None)
+                    for loc_id, loc in Locations.location_table.items()
+                    if loc["region"] == region_name and self.included_episodes[loc["episode"] - 1]
+                },
+                DOOM1993Location,
+            )
 
             self.multiworld.regions.append(region)
 
@@ -128,9 +129,8 @@ class DOOM1993World(World):
         # Forbid progression items to locations that can be missed and can't be picked up. (e.g. One-time timed
         # platform) Unless the user allows for it.
         if getattr(self.multiworld, "allow_death_logic")[self.player]:
-            self.multiworld.exclude_locations[self.player] = ExcludeLocations(Locations.death_logic_locations)
-            # self.multiworld.exclude_locations[self.player] += set(Locations.death_logic_locations)
-    
+            self.multiworld.exclude_locations[self.player].value |= set(Locations.death_logic_locations)
+
     def create_item(self, name: str) -> DOOM1993Item:
         item_id: int = self.item_name_to_id[name]
         return DOOM1993Item(name, Items.item_table[item_id]["classification"], item_id, self.player)
@@ -172,24 +172,30 @@ class DOOM1993World(World):
 
             self.multiworld.get_location(event, self.player).place_locked_item(self.create_event(event))
             self.location_count -= 1
-    
+
         # Special case for E2M6 and E3M8, where you enter a normal door then get stuck behind with a key door.
         # We need to put the key in the locations available behind this door.
         if self.included_episodes[1]:
-            self.place_locked_item_in_locations("Halls of the Damned (E2M6) - Yellow skull key", [
+            self.place_locked_item_in_locations(
                 "Halls of the Damned (E2M6) - Yellow skull key",
-                "Halls of the Damned (E2M6) - Partial invisibility 2"
-            ])
+                [
+                    "Halls of the Damned (E2M6) - Yellow skull key",
+                    "Halls of the Damned (E2M6) - Partial invisibility 2",
+                ],
+            )
         if self.included_episodes[2]:
-            self.place_locked_item_in_locations("Warrens (E3M9) - Blue skull key", [
-                "Warrens (E3M9) - Rocket launcher",
-                "Warrens (E3M9) - Rocket launcher 2",
-                "Warrens (E3M9) - Partial invisibility",
-                "Warrens (E3M9) - Invulnerability",
-                "Warrens (E3M9) - Supercharge",
-                "Warrens (E3M9) - Berserk",
-                "Warrens (E3M9) - Chaingun"
-            ])
+            self.place_locked_item_in_locations(
+                "Warrens (E3M9) - Blue skull key",
+                [
+                    "Warrens (E3M9) - Rocket launcher",
+                    "Warrens (E3M9) - Rocket launcher 2",
+                    "Warrens (E3M9) - Partial invisibility",
+                    "Warrens (E3M9) - Invulnerability",
+                    "Warrens (E3M9) - Supercharge",
+                    "Warrens (E3M9) - Berserk",
+                    "Warrens (E3M9) - Chaingun",
+                ],
+            )
 
         # Give starting levels right away
         for i in range(len(self.included_episodes)):
@@ -211,13 +217,9 @@ class DOOM1993World(World):
         self.multiworld.itempool += itempool
 
     def get_filler_item_name(self):
-        return self.multiworld.random.choice([
-            "Medikit",
-            "Box of bullets",
-            "Box of rockets",
-            "Box of shotgun shells",
-            "Energy cell pack"
-        ])
+        return self.multiworld.random.choice(
+            ["Medikit", "Box of bullets", "Box of rockets", "Box of shotgun shells", "Energy cell pack"]
+        )
 
     def create_ratioed_items(self, item_name: str, itempool: List[DOOM1993Item]):
         remaining_loc = self.location_count - len(itempool)
