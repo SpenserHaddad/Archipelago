81d07d80e452fd8170f76e0806b3f687df2a7931
Spenser Haddad <spenser.haddad@gmail.com>
2023-09-12 21:00:33 -0400

    Add completion conditions

--- worlds/brotato/Rules.py
+++ worlds/brotato/Rules.py
@@ -1,8 +1,10 @@
-from typing import TYPE_CHECKING
+from functools import partial
+from typing import Callable, TYPE_CHECKING
 
 from BaseClasses import CollectionState
 
 from .Constants import CHARACTERS
+from .Items import ItemName
 
 # from worlds.generic.Rules import set_rule
 
@@ -10,11 +12,19 @@ if TYPE_CHECKING:
     from . import BrotatoWorld
 
 
-def character_generic_completion_condition(num_required_wins: int, player: int, state: CollectionState) -> bool:
-    return state.has("Run Complete", player, count=num_required_wins)
+def _get_completion_condition(world: BrotatoWorld) -> Callable[[CollectionState], bool]:
+    num_required_wins = world.runs_required[world.player]
+    if world.brotato_win_option[world.player] == "any":
+        return partial(_completion_condition_any_character, num_required_wins, world.player)
+    else:
+        return partial(_completion_condition_specific_character, num_required_wins, world.player)
 
 
-def character_specific_completion_condition(num_required_wins: int, player: int, state: CollectionState) -> bool:
+def _completion_condition_any_character(num_required_wins: int, player: int, state: CollectionState) -> bool:
+    return state.has(ItemName.RUN_COMPLETE.value, player, count=num_required_wins)
+
+
+def _completion_condition_specific_character(num_required_wins: int, player: int, state: CollectionState) -> bool:
     won_runs = 0
     for character in CHARACTERS:
         if state.has(f"Run Complete ({character})", player):
@@ -22,7 +32,8 @@ def character_specific_completion_condition(num_required_wins: int, player: int,
     return won_runs >= num_required_wins
 
 
-def set_rules(brotato_world: "BrotatoWorld", included_episodes):
+def set_rules(world: BrotatoWorld):
+    world.multiworld.completion_condition[world.player] = _get_completion_condition(world)
     # player = brotato_world.player
     # world = brotato_world.multiworld
     pass
