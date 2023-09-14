b3bc7efc96c374e2f32a3a65a7fb51cbf4c99a9b
Spenser Haddad <spenser.haddad@gmail.com>
2023-09-13 23:02:11 -0400

    Add item for each character and helper lists

--- worlds/brotato/Items.py
+++ worlds/brotato/Items.py
@@ -9,8 +9,14 @@ from BaseClasses import Item, ItemClassification
 from .Constants import BASE_ID
 
 
+class BrotatoItem(Item):
+    game = "Brotato"
+
+
 @dataclass(frozen=True)
 class BrotatoItemBase:
+    """Hold item data before we assign to a player."""
+
     name: ItemName
     classification: ItemClassification
     code: int = field(init=False)
@@ -20,13 +26,8 @@ class BrotatoItemBase:
         object.__setattr__(self, "code", BASE_ID + BrotatoItemBase._next_offset)
         BrotatoItemBase._next_offset += 1
 
-
-class BrotatoItem(Item):
-    game = "Brotato"
-
-    @staticmethod
-    def from_item_base(base: BrotatoItemBase, player: int):
-        return BrotatoItem(base.name.value, base.classification, base.code, player)
+    def to_item(self, player: int) -> BrotatoItem:
+        return BrotatoItem(self.name.value, self.classification, self.code, player)
 
 
 class ItemName(Enum):
@@ -45,9 +46,54 @@ class ItemName(Enum):
     GOLD_50 = "Gold (50)"
     GOLD_100 = "Gold (100)"
     GOLD_200 = "Gold (200)"
-    PROGRESSIVE_CHARACTER = "Progressive Character"
     RUN_COMPLETE = "Run Complete"
+    CHARACTER_WELL_ROUNDED = "Well Rounded"
+    CHARACTER_BRAWLER = "Brawler"
+    CHARACTER_CRAZY = "Crazy"
+    CHARACTER_RANGER = "Ranger"
+    CHARACTER_MAGE = "Mage"
+    CHARACTER_CHUNKY = "Chunky"
+    CHARACTER_OLD = "Old"
+    CHARACTER_LUCKY = "Lucky"
+    CHARACTER_MUTANT = "Mutant"
+    CHARACTER_GENERALIST = "Generalist"
+    CHARACTER_LOUD = "Loud"
+    CHARACTER_MULTITASKER = "Multitasker"
+    CHARACTER_WILDLING = "Wildling"
+    CHARACTER_PACIFIST = "Pacifist"
+    CHARACTER_GLADIATOR = "Gladiator"
+    CHARACTER_SAVER = "Saver"
+    CHARACTER_SICK = "Sick"
+    CHARACTER_FARMER = "Farmer"
+    CHARACTER_GHOST = "Ghost"
+    CHARACTER_SPEEDY = "Speedy"
+    CHARACTER_ENTREPENEUR = "Entrepeneur"
+    CHARACTER_ENGINEER = "Engineer"
+    CHARACTER_EXPLORER = "Explorer"
+    CHARACTER_DOCTOR = "Doctor"
+    CHARACTER_HUNTER = "Hunter"
+    CHARACTER_ARTIFICER = "Artificer"
+    CHARACTER_ARMS_DEALER = "Arms Dealer"
+    CHARACTER_STREAMER = "Streamer"
+    CHARACTER_CYBORG = "Cyborg"
+    CHARACTER_GLUTTON = "Glutton"
+    CHARACTER_JACK = "Jack"
+    CHARACTER_LICH = "Lich"
+    CHARACTER_APPENTICE = "Appentice"
+    CHARACTER_CRYPTID = "Cryptid"
+    CHARACTER_FISHERMAN = "Fisherman"
+    CHARACTER_GOLEM = "Golem"
+    CHARACTER_KING = "King"
+    CHARACTER_RENEGADE = "Renegade"
+    CHARACTER_ONE_ARMED = "One Armed"
+    CHARACTER_BULL = "Bull"
+    CHARACTER_SOLDIER = "Soldier"
+    CHARACTER_MASOCHIST = "Masochist"
+    CHARACTER_KNIGHT = "Knight"
+    CHARACTER_DEMON = "Demon"
 
+
+_char_items = [x for x in ItemName if x.name.startswith("CHARACTER_")]
 
 _items: list[BrotatoItemBase] = [
     BrotatoItemBase(name=ItemName.COMMON_ITEM, classification=ItemClassification.filler),
@@ -65,14 +111,14 @@ _items: list[BrotatoItemBase] = [
     BrotatoItemBase(name=ItemName.GOLD_50, classification=ItemClassification.filler),
     BrotatoItemBase(name=ItemName.GOLD_100, classification=ItemClassification.filler),
     BrotatoItemBase(name=ItemName.GOLD_200, classification=ItemClassification.filler),
-    BrotatoItemBase(
-        name=ItemName.PROGRESSIVE_CHARACTER,
-        classification=ItemClassification.progression,
-    ),
     BrotatoItemBase(name=ItemName.RUN_COMPLETE, classification=ItemClassification.progression),
+    # Individual items for each character
+    *[BrotatoItemBase(name=c, classification=ItemClassification.progression) for c in _char_items],
 ]
 
 item_table = {item.code: item for item in _items}
+item_name_to_id = {item.name.value: item.code for item in _items}
+filler_items = [item.name.value for item in item_table.values() if item.classification == ItemClassification.filler]
 
 item_name_groups = {
     "Item Drops": {
@@ -94,5 +140,5 @@ item_name_groups = {
         ItemName.GOLD_100.value,
         ItemName.GOLD_200.value,
     },
-    "Characters": {ItemName.PROGRESSIVE_CHARACTER.value},
+    "Characters": set(c.value for c in _char_items),
 }
