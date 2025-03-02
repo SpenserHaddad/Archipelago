84537cbce572950232b3e88cf12875e459e5ee83
Spenser Haddad <spenser.haddad@gmail.com>
2025-03-02 13:48:18 -0500

    Add starting characters to pool first to ensure they're selected properly

--- worlds/brotato/characters.py
+++ worlds/brotato/characters.py
@@ -1,9 +1,16 @@
 import random
+from collections.abc import Sequence
 from typing import NamedTuple
 
 from Options import OptionError
 
-from .constants import ABYSSAL_TERRORS_CHARACTERS, BASE_GAME_CHARACTERS, CharacterGroup
+from .constants import (
+    ABYSSAL_TERRORS_CHARACTERS,
+    ALL_CHARACTERS,
+    BASE_GAME_CHARACTERS,
+    CHARACTER_GROUPS,
+    CharacterGroup,
+)
 from .options import StartingCharacters
 
 
@@ -40,49 +47,77 @@ def get_available_and_starting_characters(
         ),
     }
 
-    included_characters: list[str] = []
+    valid_characters: set[str] = set()
     for pack_enabled, include_characters_from_pack, pack_characters in character_pack_info.values():
         if pack_enabled:
-            included_characters = [c for c in pack_characters.characters if c in include_characters_from_pack]
+            valid_characters |= {c for c in pack_characters.characters if c in include_characters_from_pack}
 
-    # Sort the characters so random selection is deterministic, then pick who to include to match the requested amount.
-    included_characters.sort()
-    available_characters = random.sample(included_characters, num_available_characters)
+    # Pick characters from the starting character pool first, then add more others until we have the requested amount.
+    all_starting_characters_for_option: list[str] = get_starting_characters_for_option(
+        starting_character_mode, enable_abyssal_terrors_dlc
+    )
+    valid_starting_characters: list[str] = [
+        char for char in all_starting_characters_for_option if char in valid_characters
+    ]
+    if not valid_starting_characters:
+        options_str = ", ".join(
+            [
+                f"{enable_abyssal_terrors_dlc=}",
+                f"{starting_character_mode=}",
+                f"{num_starting_characters=}",
+                f"{include_base_game_characters=}",
+                # Put these two last because they can generate very long entries
+                f"{num_available_characters=}",
+                f"{include_abyssal_terrors_characters=}",
+            ]
+        )
+        raise OptionError(f"No valid starting characters for given options: {options_str}")
 
-    starting_characters: list[str]
+    num_starting_characters_to_select = min(num_starting_characters, len(valid_starting_characters))
+    starting_characters: list[str] = random.sample(valid_starting_characters, num_starting_characters_to_select)
+
+    available_characters: list[str] = starting_characters.copy()
+    num_characters_to_add = num_available_characters - len(available_characters)
+    if num_characters_to_add > 0:
+        valid_characters_to_add = sorted(valid_characters - set(starting_characters))
+        available_characters += random.sample(valid_characters_to_add, num_characters_to_add)
+    return CharacterInfoOutput(available_characters=available_characters, starting_characters=starting_characters)
+
+
+def get_starting_characters_for_option(
+    starting_character_mode: StartingCharacters, enable_abyssal_terrors_dlc: bool
+) -> list[str]:
+    """Returns a sorted list of the possible starting characters for the given starting character mode.
+
+    This naively returns ALL characters for the mode, ignoring the include_*_characters options. It's assumed that the
+    caller will filter the output list down to what's allowed from the other options.
+
+    Returns a sorted list of the valid starting characters. The output is sorted to guarantee deterministic random
+    selection.
+    """
+    starting_characters: Sequence[str]
     match starting_character_mode.value:
         case StartingCharacters.option_default_all:
-            enabled_groups: list[CharacterGroup] = [pack.group for pack in character_pack_info.values() if pack.enabled]
-            starting_characters = [
-                char for eg in enabled_groups for char in eg.default_characters if char in available_characters
-            ]
+            starting_characters = [char for group in CHARACTER_GROUPS.values() for char in group.default_characters]
         case StartingCharacters.option_random_all:
-            starting_characters = random.sample(available_characters, num_starting_characters)
+            starting_characters = ALL_CHARACTERS
         case StartingCharacters.option_default_base_game:
-            starting_characters = [
-                char for char in BASE_GAME_CHARACTERS.default_characters if char in available_characters
-            ]
+            starting_characters = BASE_GAME_CHARACTERS.default_characters
         case StartingCharacters.option_random_base_game:
-            starting_characters = random.sample(
-                sorted(character_pack_info[BASE_GAME_CHARACTERS.name].include_characters), num_starting_characters
-            )
+            starting_characters = BASE_GAME_CHARACTERS.characters
         case StartingCharacters.option_default_abyssal_terrors:
             if not enable_abyssal_terrors_dlc:
                 raise OptionError(
                     f"Starting option set to {starting_character_mode}, but Abyssal Terrors DLC is disabled."
                 )
-            starting_characters = [
-                char for char in ABYSSAL_TERRORS_CHARACTERS.default_characters if char in available_characters
-            ]
+            starting_characters = ABYSSAL_TERRORS_CHARACTERS.default_characters
         case StartingCharacters.option_random_abyssal_terrors:
             if not enable_abyssal_terrors_dlc:
                 raise OptionError(
                     f"Starting option set to {starting_character_mode}, but Abyssal Terrors DLC is disabled."
                 )
-            starting_characters = random.sample(
-                sorted(character_pack_info[ABYSSAL_TERRORS_CHARACTERS.name].include_characters), num_starting_characters
-            )
+            starting_characters = ABYSSAL_TERRORS_CHARACTERS.characters
         case _:
             raise OptionError(f"Unknown value for starting character option: {starting_character_mode}")
 
-    return CharacterInfoOutput(available_characters=available_characters, starting_characters=starting_characters)
+    return sorted(starting_characters)
